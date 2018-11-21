34cd0a627b1a051410971d0c4d94cdfa0cafa6f0
anatolyefimov <efimo.tolik@gmail.com>
2018-11-21 14:13:19 +0300

    added filters layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,5 @@ pagination(document.querySelector('.scenario'), 1);
 let menu = document.querySelector('.menu')
 document.querySelector('.icon-list').addEventListener('click', function() {
     menu.style.height = (menu.style.height === '0px') ? '90px' : '0px';
+    
 })
