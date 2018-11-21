7f6709ca7eb790d13613f1dbc28536ed42b24745
anatolyefimov <efimo.tolik@gmail.com>
2018-11-21 13:25:19 +0300

    menu on mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 pagination(document.querySelector('.devices'), 215)
 pagination(document.querySelector('.scenario'), 1);
 
+let menu = document.querySelector('.menu')
+document.querySelector('.icon-list').addEventListener('click', function() {
+    menu.style.height = (menu.style.height === '0px') ? '90px' : '0px';
+})
