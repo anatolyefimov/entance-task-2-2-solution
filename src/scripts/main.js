5ed332fa5790c5166a06ce13806bc725f9ccd64b
anatolyefimov <efimo.tolik@gmail.com>
2018-11-22 10:51:03 +0300

    filer list orintation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,6 @@ pagination(document.querySelector('.scenario'), 1);
 let menu = document.querySelector('.menu')
 document.querySelector('.icon-list').addEventListener('click', function() {
     menu.style.height = (menu.style.height === '0px') ? '90px' : '0px';
-    
 })
 
 let filters = document.querySelector('.filters').children;
